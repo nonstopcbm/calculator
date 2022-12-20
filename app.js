8b20d9254c2e37fbc2089d728cbd9b7004f88f85
Chris Million <92188236+nonstopcbm@users.noreply.github.com>
2022-12-20 15:51:28 -0600

    Added styling to form the calculator

---
+++ app.js
@@ -0,0 +1,24 @@
+// addition function
+function toAdd(a, b) {
+    return a + b
+}
+
+// substraction function
+function toSubtract(a, b) {
+    return a - b
+}
+
+// multiplication function
+function toMultiply(a, b) {
+    return a * b
+}
+
+// division function
+function toDivide(a, b) {
+    return a / b
+}
+
+// operation function
+function operate() {
+    
+}
